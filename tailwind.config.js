7c0baca3f9d0aac2f9504a6d69ce6b3a436c50da
Rafael Poveda <rafael.poveda@cornershopapp.com>
2023-07-03 11:34:27 -0500

    fix: remove tailwind.config.js from gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [`${process.cwd()}/templates/*.{html,ts}`],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
